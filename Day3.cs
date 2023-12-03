8ba107930a4ef14f5c00bb925b0181bbb15688ee
Wojtek <mkwojtek@wp.pl>
2023-12-03 13:09:36 +0100

    Restructure project

---
+++ Day3.cs
@@ -0,0 +1,14 @@
+﻿namespace Advent23;
+
+internal class Day3 : IDay
+{
+	public void Star1()
+	{
+		throw new NotImplementedException();
+	}
+
+	public void Star2()
+	{
+		throw new NotImplementedException();
+	}
+}
